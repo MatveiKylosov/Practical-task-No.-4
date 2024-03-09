47c7a203ad6645aa015934aea1dde90a1df5273e
MatveiKylosov <matveikyl@mail.ru>
2024-03-09 18:11:58 +0500

    Реализованный переход. Kylosov

--- app/src/main/java/com/example/practicaltaskno4/MainActivity.java
+++ app/src/main/java/com/example/practicaltaskno4/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.practicaltaskno4;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onStepAge(View view){
+
+    }
+
+    public void onStepSex(View view){
+
+    }
+
+    public void onStepEmail(View view){
+
     }
 }
